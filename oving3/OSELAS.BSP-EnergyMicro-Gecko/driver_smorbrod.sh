6347cbc6005c49d112dc2bcd61bae39fdbbdbdf5
TDT4258 Student <tdt4258@tdt4258-VirtualBox.(none)>
2016-11-17 20:56:35 +0100

    add smorbrodliste

---
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/driver_smorbrod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ptxdist clean driver-gamepad
+ptxdist compile driver-gamepad
+ptxdist targetinstall driver-gamepad
+ptxdist targetinstall kernel
+ptxdist image root.romfs
+ptxdist test flash-rootfs
