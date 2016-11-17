f3828068ad757a29da0250f850afe45cb0edbebb
Ludvig Jordet <ludvig.jordet@gmail.com>
2016-11-17 18:36:31 +0100

    write device driver skeleton

--- oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,6 +5,9 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/cdev.h>
 
 /*
  * template_init - function to insert this module into kernel space
@@ -15,9 +18,56 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int __init template_init(void)
+
+static int my_open(struct inode *inode, struct file *filp){
+
+}
+
+static int my_release(struct inode *inode, struct file *filp){
+
+}
+
+static ssize_t my_read(struct file *filp, char __user *buff, size_t count,
+        loff_t *offp){
+    return count;
+}
+
+static ssize_t my_write(struct file *filp, const char __user *buff,
+        size_t count, loff_t *offp){
+    printk("Du skrev ting til driveren. Driveren vet ikke helt hva den synes om dette\n");
+    return count;
+}
+
+static struct file_operations my_fops = {
+    .owner = THIS_MODULE,
+    .read = my_read,
+    .write = my_write,
+    .open = my_open,
+    .release = my_release
+};
+
+dev_t device;
+struct cdev my_cdev;
+struct class *cl;
+
+static int __init my_init(void)
 {
-	printk("Hello World, here is your module speaking\n");
+    int error = 0;
+	
+    printk("Loading gamepad driver.\nAllocating chrdev_region...\n");
+    if(error = alloc_chrdev_region(&device, 0, 1, "gamepad"))
+        return error;
+
+    printk("Initializing cdev struct...\n");
+    cdev_init(&my_cdev, &my_fops);
+
+    printk("Adding cdev...\n");
+    if(error = cdev_add(&my_cdev, device, 1))
+        return error;
+
+    printk("Class/device create...\n");
+    cl = class_create(THIS_MODULE, "gamepad");
+    device_create(cl, NULL, device, NULL, "gamepad");
 	return 0;
 }
 
@@ -28,13 +78,14 @@ static int __init template_init(void)
  * code from a running kernel
  */
 
-static void __exit template_cleanup(void)
+static void __exit my_exit(void)
 {
-	 printk("Short life for a small module...\n");
+	 printk("Unloading gamepad driver...\n");
+     unregister_chrdev_region(device, 1);
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+module_init(my_init);
+module_exit(my_exit);
 
 MODULE_DESCRIPTION("Small module, demo only, not very useful.");
 MODULE_LICENSE("GPL");
